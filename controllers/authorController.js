a55542a8af7f0dc914adb941ff834b63620a893d
Michaela Wagner <mickywagner@gmail.com>
2020-04-18 18:36:03 -0600

    Add controller files and functions to author controller

---
+++ controllers/authorController.js
@@ -0,0 +1,45 @@
+var Author = require('../models/author')
+
+// Display list of all authors
+exports.author_list = function(req, res ){
+    res.send('NOT IMPLEMENTED: Author list')
+}
+
+// Display detail page for a specific Author
+exports.author_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id)
+}
+
+// Display author create form on GET
+exports.author_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create GET')
+}
+
+// Handle Author create on POST
+exports.authors_create_post = function(req, res) {
+    exports.author_create_post = function(req, res) {
+        res.send('NOT IMPLEMENTED: Author create POST')
+    }
+}
+
+// Display Author delete form on GET
+exports.author_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author delete GET')
+}
+
+// Display Author delete from POST
+exports.author_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author delete POST')
+}
+
+// Display Author update form on GET
+
+exports.author_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update GET')
+}
+
+// Handle Author update on POST
+
+exports.author_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author update POST')
+}
