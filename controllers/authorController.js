91b3f0e8e729061ef7cf39ee3cbe9170bdf4a5ec
Michaela Wagner <mickywagner@gmail.com>
2020-04-22 23:23:51 -0600

    Create author GET/POST method and author form view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,8 @@
 var Author = require('../models/author')
 var Book = require('../models/book')
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
+
 
 var async = require('async')
 
@@ -40,14 +43,47 @@ exports.author_detail = function(req, res) {
 }
 
 // Display author create form on GET
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET')
+exports.author_create_get = function(req, res, next) {
+    res.render('author_form', { title: 'Create Author'})
 }
 
 // Handle Author create on POST
-exports.author_create_post = function(req, res) {
-        res.send('NOT IMPLEMENTED: Author create POST')
-}
+exports.author_create_post = [
+    body('first_name').isLength({min: 1}).trim().withMessage('First name must be specified')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters'),
+    body('family_name').isLength({min: 1}).trim().withMessage('Family name must be specified')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true}).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true}).isISO8601(),
+
+    sanitizeBody('first_name').escape(),
+    sanitizeBody('last_name').escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array()})
+            return
+        } else {
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                }
+            )
+            author.save(function(err) {
+                if(err) {return next(err)}
+
+                res.redirect(author.url)
+            })
+        }
+    }
+]
 
 
 // Display Author delete form on GET
