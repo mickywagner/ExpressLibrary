1540fef8a04b1774d8225842cba3b35791c912c7
Michaela Wagner <mickywagner@gmail.com>
2020-04-24 20:26:29 -0600

    Add helmet for headers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog')
 var compression = require('./compression')
+var helmet = require('helmet')
 
 var app = express();
 
@@ -22,6 +23,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
