54dbc5bec27505782dae938ffbd2d127beb4985c
Michaela Wagner <mickywagner@gmail.com>
2020-04-24 20:36:07 -0600

    Prepare for Heroku deploy

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var helmet = require('helmet')
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://MickyWagner:MRP00PYbutth0le@cluster0-qr8pe.gcp.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://MickyWagner:MRP00PYbutth0le@cluster0-qr8pe.gcp.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, { useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '))
